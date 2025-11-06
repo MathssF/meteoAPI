de3165b2af89f8f5a4e6333f8fe245277f75181f
MathssF <102183898+MathssF@users.noreply.github.com>
2025-11-06 19:51:58 +0000

    fix module

--- src/alert/alert.module.ts
+++ src/alert/alert.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AlertService } from './alert.service';
 import { AlertController } from './alert.controller';
+import { TriggeredAlertService } from './triggered-alert.service';
+import { TriggeredAlertController } from './triggered-alert.controller';
 
 @Module({
-  controllers: [AlertController],
-  providers: [AlertService],
+  controllers: [AlertController, TriggeredAlertController],
+  providers: [AlertService, TriggeredAlertService],
 })
 export class AlertModule {}
