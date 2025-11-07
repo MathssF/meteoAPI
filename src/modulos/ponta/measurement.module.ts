369ab6842d2ad0b0f50cf46b340b2473356997bc
MathssF <102183898+MathssF@users.noreply.github.com>
2025-11-07 05:09:19 +0000

    Adicionando a nova service na module

--- src/modulos/ponta/measurement.module.ts
+++ src/modulos/ponta/measurement.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MeasurementPostService } from '../../services/measurement.post.service';
 import { MeasurementFetchService } from '../../services/measurement.fetchs.service';
+import { MeasurementRandomService } from 'src/services/measurement.random.service';
 import { MeasurementController } from '../../controllers/measurement.controller';
 
 @Module({
   controllers: [MeasurementController],
-  providers: [MeasurementPostService, MeasurementFetchService],
-  exports: [MeasurementPostService, MeasurementFetchService],
+  providers: [MeasurementPostService, MeasurementFetchService, MeasurementRandomService],
+  exports: [MeasurementPostService, MeasurementFetchService, MeasurementRandomService],
 })
 export class MeasurementModule {}
