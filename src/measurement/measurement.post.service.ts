73c4a080744e890a91c0c24097849fb1043f569e
MathssF <102183898+MathssF@users.noreply.github.com>
2025-11-07 02:13:33 +0000

    Note: Iniciando agora os testes

--- src/measurement/measurement.post.service.ts
+++ src/measurement/measurement.post.service.ts
@@ -5,27 +5,35 @@ import { findValidParameters } from './utils/parameter.utils';
 import { fetchMeteomaticsData } from './utils/meteomatics.utils';
 import { findOrCreateLocations,  } from './utils/local.utils';
 import { processAndSaveMeasurements, scheduleMeasurement } from './utils/measurement.utils';
+import { checkAlerts } from './utils/alerts.utils';
 
 @Injectable()
 export class MeasurementPostService {
   constructor(private prisma: PrismaService) {}
 
   async getAndPost(dto: FetchMeasurementsDto) {
-  const username = process.env.METEOMATICS_USER;
-  const password = process.env.METEOMATICS_PASS;
-  if (!username || !password) throw new Error('Prtecisa das credenciais');
+    const username = process.env.METEOMATICS_USER;
+    const password = process.env.METEOMATICS_PASS;
+    if (!username || !password) throw new Error('Prtecisa das credenciais');
 
-  const date = dto.date ?? new Date().toISOString().split('.')[0] + 'Z';
-  const { parameters, invalidParameters } = await findValidParameters(this.prisma, dto.parameters);
-  if (parameters.length === 0) return { status: 'error', message: 'Nenhum parâmetro válido encontrado.', invalidParameters };
+    const date = dto.date ?? new Date().toISOString().split('.')[0] + 'Z';
+    const { parameters, invalidParameters } = await findValidParameters(this.prisma, dto.parameters);
+    if (parameters.length === 0) return { status: 'error', message: 'Nenhum parâmetro válido encontrado.', invalidParameters };
 
-  const locations = await findOrCreateLocations(this.prisma, dto.locations);
-  const paramCodes = parameters.map(p => p.code).join(',');
-  const coordString = locations.map(l => `${l.lat},${l.lon}`).join('+');
+    const locations = await findOrCreateLocations(this.prisma, dto.locations);
+    const paramCodes = parameters.map(p => p.code).join(',');
+    const coordString = locations.map(l => `${l.lat},${l.lon}`).join('+');
 
-  const meteomaticsData = await fetchMeteomaticsData(username, password, date, paramCodes, coordString);
-  const batch = await this.prisma.forecastBatch.create({ data: { source: 'meteomatics' } });
-  const savedMeasurements = await processAndSaveMeasurements(this.prisma, meteomaticsData.data, parameters, locations, batch.id);
+    const meteomaticsData = await fetchMeteomaticsData(username, password, date, paramCodes, coordString);
+    const batch = await this.prisma.forecastBatch.create({ data: { source: 'meteomatics' } });
+    const savedMeasurements = await processAndSaveMeasurements(this.prisma, meteomaticsData.data, parameters, locations, batch.id);
+
+    let matchAlerts: any[] = []; // CheckAlertResult[][]
+
+    savedMeasurements.forEach(async (elem) => {
+      let checkAtual = await checkAlerts(this.prisma, elem.localId, elem.parameterId, elem.value);
+      if (checkAtual) matchAlerts.push(checkAtual);
+    })
 
     return {
       status: 'ok',
@@ -33,6 +41,7 @@ export class MeasurementPostService {
       savedCount: savedMeasurements.length,
       response: savedMeasurements,
       invalidParameters,
+      matchAlerts,
     };
   }
 
--- src/measurement/measurement.service.spec.ts
+++ src/measurement/measurement.service.spec.ts
@@ -3,17 +3,18 @@ import { MeasurementPostService } from './measurement.post.service';
 import { MeasurementFetchService } from './measurement.fetchs.service';
 
 describe('MeasurementService', () => {
-  let service: MeasurementPostService;
+  let postService: MeasurementPostService;
+  let fetchService: MeasurementFetchService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MeasurementPostService],
     }).compile();
 
-    service = module.get<MeasurementPostService>(MeasurementPostService);
+    postService = module.get<MeasurementPostService>(MeasurementPostService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(postService).toBeDefined();
   });
 });
