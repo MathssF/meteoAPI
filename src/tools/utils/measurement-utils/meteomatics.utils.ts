26697529e8533172b27799b0c2832e72e82574f2
MathssF <102183898+MathssF@users.noreply.github.com>
2025-11-07 16:15:38 +0000

    clear logs

--- src/tools/utils/measurement-utils/meteomatics.utils.ts
+++ src/tools/utils/measurement-utils/meteomatics.utils.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export async function fetchMeteomaticsData(username: string, password: string, date: string, paramCodes: string, coordString: string) {
-  console.log('meteomatics utils, coord: ', coordString);
   const url = `https://api.meteomatics.com/${date}/${paramCodes}/${coordString}/json`;
   const response = await axios.get(url, { auth: { username, password } });
   return response.data;
