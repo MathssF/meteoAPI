0aa0788448dbf2237a8162c091c066b8e58c8639
MathssF <102183898+MathssF@users.noreply.github.com>
2025-11-07 03:53:57 +0000

    Test - Mocks

--- src/tools/tests/basic.spec.ts
+++ src/tools/tests/basic.spec.ts
@@ -1,1 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { LocalService } from '../../services/local.service';
+import { ParameterService } from '../../services/parameter.service';
+import { LocalController } from '../../controllers/local.controller';
+import { ParameterController } from '../../controllers/parameter.controller';
+import { PrismaService } from '../../core/data/prisma/prisma.service';
+
+const prismaMock = {
+  local: {
+    create: jest.fn(),
+    findUnique: jest.fn(),
+    findMany: jest.fn(),
+  },
+  parameter: {
+    create: jest.fn(),
+    findUnique: jest.fn(),
+    findMany: jest.fn(),
+  },
+};
