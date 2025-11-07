fdda194b150cd98d3ccb5895f277512178833f4f
MathssF <102183898+MathssF@users.noreply.github.com>
2025-11-07 05:58:01 +0000

    measurement

--- src/tools/tests/measurement.spec.ts
+++ src/tools/tests/measurement.spec.ts
@@ -6,6 +6,10 @@ import { MeasurementFetchService } from 'src/services/measurement.fetchs.service
 import { MeasurementPostService } from 'src/services/measurement.post.service';
 import { MeasurementRandomService } from 'src/services/measurement.random.service';
 import { MeasurementController } from 'src/controllers/measurement.controller';
+import { ScheduleService } from 'src/services/schedule.service';
+import { ScheduleController } from 'src/controllers/schedule.controller';
+import { AlertService } from 'src/services/alert.service';
+import { AlertController } from 'src/controllers/alert.controller';
 
 describe('', () => {
   
