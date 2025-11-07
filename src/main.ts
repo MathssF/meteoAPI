015ceb365c1324b3242e6e76fd18c6d98b142057
MathssF <102183898+MathssF@users.noreply.github.com>
2025-11-07 15:01:47 +0000

    Docs Swagger no Main, e aplicação do dot env

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modulos/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+const n: string = process.env.NAME || '';
+const u: string = process.env.URL || '';
+const m: string = process.env.EMAIL || '';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('MeteoAPI')
     .setDescription('Uma API que confere a meteomatics, salva os dados na persistência, e apresenta outras funcionalidades!')
-    .setContact('Matheus Flores', 'https://www.linkedin.com/in/matheus-oaf/', 'maths.aki@gmail.com')
+    .setContact(n, u, m)
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
