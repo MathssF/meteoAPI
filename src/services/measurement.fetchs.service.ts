1038ccfd94fb4e8f8514d03bb10d7b468575fc53
MathssF <102183898+MathssF@users.noreply.github.com>
2025-12-10 21:08:52 +0000

    MoreInfosInAPI

--- src/services/measurement.fetchs.service.ts
+++ src/services/measurement.fetchs.service.ts
@@ -8,10 +8,17 @@ export class MeasurementFetchService {
   async find(filters: { id?: string; batchId?: string; scheduleId?: string }) {
     const { id, batchId, scheduleId } = filters;
 
+    const baseInclude = {
+      local: { select: { id: true, name: true, lat: true, lon: true } },
+      parameter: { select: { id: true, code: true, name: true, unit: true } },
+      batch: true,
+      schedule: true,
+    };
+
     if (id) {
       const measurement = await this.prisma.measurement.findUnique({
         where: { id },
-        include: { local: true, parameter: true, batch: true, schedule: true },
+        include: baseInclude, // { local: true, parameter: true, batch: true, schedule: true }
       });
       if (!measurement)
         throw new NotFoundException(`Measurement ${id} não encontrada`);
