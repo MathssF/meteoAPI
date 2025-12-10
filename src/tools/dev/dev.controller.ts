f2d5db366682ee78ad965e5b6f80bc11049f0132
MathssF <102183898+MathssF@users.noreply.github.com>
2025-12-10 22:45:58 +0000

    Entregue

--- src/tools/dev/dev.controller.ts
+++ src/tools/dev/dev.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Get, Param, Patch, Body, Put } from '@nestjs/common';
 import { DevService } from './dev.service';
 import type { DevCheckDto } from '../dto/dev.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('dev')
 export class DevController {
@@ -8,12 +9,21 @@ export class DevController {
 
   // ---- START ----
   @Post('start')
+  @ApiOperation({
+      summary: 'Iniciar ou Reiniciar o Banco de Dados',
+      description:
+        'Possui os dados iniciais que serão usados.',
+    })
   async start() {
     await this.devService.start();
     return { message: 'Banco Iniciado, ou Reiniciado' };
   }
 
-  @Get('health')
+  @Get('health')@ApiOperation({
+    summary: 'Conferir conexões',
+    description:
+      'Verifica a conexão com o Banco, e com a API da Meteo, e informa a latência.',
+  })
   async getHealth() {
     return this.devService.getHealthStatus();
   }
