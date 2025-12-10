c5331dc8fa6ef11cea43dea5f65bd699de1cf10d
MathssF <102183898+MathssF@users.noreply.github.com>
2025-12-10 22:34:53 +0000

    more

--- src/tools/dev/dev.service.ts
+++ src/tools/dev/dev.service.ts
@@ -8,7 +8,20 @@ export class DevService {
 
   // ---- START ----
   async start() {
+    await this.prisma.$executeRawUnsafe(`SET FOREIGN_KEY_CHECKS = 0;`);
+
+    const tables = await this.prisma.$queryRaw<
+      Array<{ TABLE_NAME: string }>
+    >`SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'meteo_db'`;
+
+    for (const t of tables) {
+      await this.prisma.$executeRawUnsafe(`TRUNCATE TABLE ${t.TABLE_NAME}`);
+    }
+
+    await this.prisma.$executeRawUnsafe(`SET FOREIGN_KEY_CHECKS = 1;`);
     await mainSeed(this.prisma);
-    console.log('Seeds processadas')
+
+    return { message: 'Banco resetado com sucesso!' };
   }
+
 }
