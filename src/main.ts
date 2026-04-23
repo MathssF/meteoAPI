ef759fafe2b9e03cda23bc186441d701504e4027
Matheus Flores <Maths.Aki@gmail.com>
2026-04-23 12:54:44 -0300

    Fix Dockerfile prisma copy and use Render port

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || process.env.RENDER ? 10000 : 3000;
+  await app.listen(port);
 }
 bootstrap();
